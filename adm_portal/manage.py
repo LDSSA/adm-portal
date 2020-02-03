0c7e2d935facec3adba3bf24a240c9c19161c3a1
spin14 <acci.valverde@gmail.com>
2020-02-03 21:02:08 +0000

    update manage.py

--- adm_portal/manage.py
+++ adm_portal/manage.py
@@ -1,11 +1,13 @@
+import os
 import sys
 
 from django.core.management import execute_from_command_line
 
-
-def main() -> None:
-    execute_from_command_line(sys.argv)
-
-
 if __name__ == "__main__":
-    main()
+    if os.environ.get("DJANGO_SETTINGS_MODULE") is None:
+        raise SystemExit(
+            "Error: `DJANGO_SETTINGS_MODULE` is not set!\n"
+            "Set it by running `export DJANGO_SETTINGS_MODULE=adm_portal.settings.dev`"
+        )
+
+    execute_from_command_line(sys.argv)
