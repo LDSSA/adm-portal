a1baf1e2909cbdb1ebc0a26b78600418316a7f30
spin14 <acci.valverde@gmail.com>
2020-02-15 17:40:52 +0000

    allow any host

--- adm_portal/adm_portal/settings/base.py
+++ adm_portal/adm_portal/settings/base.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS: typing.List[str] = []
+ALLOWED_HOSTS: typing.List[str] = ["*"]
 
 
 # Application definition
