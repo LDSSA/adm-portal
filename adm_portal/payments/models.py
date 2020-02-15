24bec123e368887e50f2b2534dbff433f514b12f
dominguezmaria <mariacristinavfdominguez@gmail.com>
2020-02-15 20:45:53 +0000

    Add payment status on candidate page

--- adm_portal/payments/models.py
+++ adm_portal/payments/models.py
@@ -2,6 +2,7 @@ import os
 from logging import getLogger
 
 from django.db import models
+from profiles.models import Profile
 
 logger = getLogger(__name__)
 
@@ -37,22 +38,65 @@ class Payment(models.Model):
     status = models.CharField(
         blank=False,
         null=False,
-        max_length=10,
-        choices=[("pending", "Pending"), ("accepted", "Accepted"), ("rejected", "Rejected")],
+        max_length=30,
+        default="waiting_for_documents",
+        choices=[
+            ("waiting_for_documents", "Waiting for candidate documents"),
+            ("pending_verification", "Pending verification"),
+            ("accepted", "Accepted"),
+            ("rejected", "Rejected")
+        ],
     )
 
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def add_document(self, document: Document) -> None:
-        logger.info(f"payment_id={self.id}: new document uploaded")
-        document.payment = self
-        document.save()
 
     @property
     def get_payment_proof_documents(self):
         return Document.objects.filter(payment=self, doc_type="payment_proof").order_by("-updated_at")
 
     @property
+    def has_payment_proof_document(self):
+        return len(self.get_payment_proof_documents) > 0
+    
+    @property
     def get_student_id_documents(self):
         return Document.objects.filter(payment=self, doc_type="student_id").order_by("-updated_at")
+
+    @property
+    def has_student_id_document(self):
+        return len(self.get_student_id_documents) > 0
+
+    @property
+    def is_status_accepted(self):
+        return self.status == "accepted"
+    
+    @property
+    def is_status_rejected(self):
+        return self.status == "rejected"
+    
+    @property
+    def is_status_pending_verification(self):
+        return self.status == "pending_verification"
+
+    @property
+    def is_status_waiting_for_documents(self):
+        return self.status == "waiting_for_documents"
+    
+    def add_document(self, document: Document) -> None:
+        logger.info(f"payment_id={self.id}: new document uploaded")
+        document.payment = self
+        document.save()
+
+        # now we need to update the payment status to "pending_verification"
+        # unless the candidate is a student and is missing the other proof (payment proof or student id)
+        profile = Profile.objects.get(user=self.user)
+        
+        if profile.is_student:
+            has_missing_student_id = document.doc_type == "payment_proof" and not self.has_student_id_document
+            has_missing_payment_proof = document.doc_type == "student_id" and not self.has_payment_proof_document
+            if has_missing_student_id or has_missing_payment_proof:
+                return
+
+        self.status = "pending_verification"
+        self.save()
